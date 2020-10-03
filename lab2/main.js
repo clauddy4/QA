32667103a4a2cb7f503994dc78f7899c0e42f040
clauddy <claudemakarova@gmail.com>
2020-10-03 10:31:02 +0300

    update lab 2

--- lab2/main.js
+++ lab2/main.js
@@ -8,54 +8,48 @@ const URL = `${BASE_URL}/broken-links`;
 
 const processed = new Set();
 
-const counters = {
-    err: 0,
-    ok: 0,
-};
+let valid = 0;
+let invalid = 0;
 
 async function main() {
     await testHtmlLinks(URL + "/");
 
-    const now = new Date().toISOString();
+    const date = new Date().toISOString();
 
-    console.log(`\n${counters.ok}\n${now}`);
-    console.error(`\n${counters.err}\n${now}`);
+    console.log(`\n${valid}\n${date}`);
+    console.error(`\n${invalid}\n${date}`);
 }
 
 async function testHtmlLinks(url) {
     const options = {
-        encoding: "utf8",
-        timeout: 5000,
-        resolveWithFullResponse: true,
+        resolveWithFullResponse: true
     };
+
     try {
         const response = await request(url, options);
-        counters.ok++;
+        valid++;
         console.log(`${response.statusCode}\t${url}`);
-        await runTestsDeep(response.body);
+        await runTest(response.body);
     } catch (error) {
-        counters.err++;
+        invalid++;
         console.error(`${error.statusCode}\t${url}`);
     }
 }
 
-async function runTestsDeep(content) {
+async function runTest(content) {
     const { window } = new JSDOM(content);
     const { document } = window;
     const nodes = document.getElementsByTagName("a");
     const links = getLinksFromNodes(nodes);
+    
     for (const link of links) {
         const url = `${URL}/${link}`;
-        // check if links already processed
         if (isLinkProcessed(url) || isLinkProcessed(link)) {
             continue;
         }
-        // mark as processed
         markLinkProcessed(link);
         markLinkProcessed(url);
-        // recursion starts here
         if (link.startsWith("http")) {
-            // skip any third-party URL
             if (isThirdPartyUrl(link)) {
                 continue;
             }
